9930bcdbc542f8aef056cbc02285037a1d022ccd
HarryPotter-4000 <80061297+HarryPotter-4000@users.noreply.github.com>
2021-09-20 18:19:31 +0300

    greetings

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 let currentScrollPos = window.pageYOffset;
 	if (prevScrollpos < currentScrollPos) {
+		// document.getElementById("navbar").style.top = "-86px";
 		document.getElementById("navbar").classList.add('navbar--hidden');
 	} else {
+		// document.getElementById("navbar").style.top = "0";
+		// document.getElementById("navbar").style.top = "0";
 		document.getElementById("navbar").classList.remove('navbar--hidden');
 	}
 	prevScrollpos = currentScrollPos;
